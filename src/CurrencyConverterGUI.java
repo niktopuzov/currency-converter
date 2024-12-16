ab62deaa6154b5adee61816b0d427c74e1d0201f
Nikola Topuzov <nik.topuzov@gmail.com>
2024-12-16 10:17:49 +0200

    initial setup

--- src/CurrencyConverterGUI.java
+++ src/CurrencyConverterGUI.java
@@ -1,4 +1,8 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public class CurrencyConverterGUI {
-
+public class CurrencyConverterGUI extends JFrame{
+	
+	JButton btnCalculate = new JButton("Calculate");
+	JButton btnDelete = new JButton("Delete");
 }
---
+++ src/MainClass.java
@@ -0,0 +1,6 @@
+
+public class MainClass {
+	public static void main(String[] args) {
+		CurrencyConverterGUI frame = new CurrencyConverterGUI();
+	}
+}
