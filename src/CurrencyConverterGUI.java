9ba8a962af1a32ccd4b5afc76ee0f07c73cf386b
Nikola Topuzov <nik.topuzov@gmail.com>
2024-12-16 10:30:08 +0200

    add text fields for euro, usd and exchange rate

--- src/CurrencyConverterGUI.java
+++ src/CurrencyConverterGUI.java
@@ -1,8 +1,13 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class CurrencyConverterGUI extends JFrame{
 	
 	JButton btnCalculate = new JButton("Calculate");
 	JButton btnDelete = new JButton("Delete");
+	
+	JTextField textFieldEURO = new JTextField();
+	JTextField textFieldUSD = new JTextField();
+	JTextField textFieldExchangeRate = new JTextField("0.95");
 }
