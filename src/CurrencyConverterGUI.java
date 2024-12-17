a50eda6653717d1bb8de27063f9b3eb03e5e5e0f
Nikola Topuzov <nik.topuzov@gmail.com>
2024-12-17 10:02:54 +0200

    add currency labels and exchange rate label

--- src/CurrencyConverterGUI.java
+++ src/CurrencyConverterGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CurrencyConverterGUI extends JFrame{
@@ -10,4 +12,13 @@ public class CurrencyConverterGUI extends JFrame{
 	JTextField textFieldEURO = new JTextField();
 	JTextField textFieldUSD = new JTextField();
 	JTextField textFieldExchangeRate = new JTextField("0.95");
+	
+	JLabel euroL = new JLabel("Euro");
+	JLabel usdL = new JLabel("USD");
+	JLabel exchangeRateL = new JLabel("Exchange Rate");
+	
+	JPanel panel = new JPanel();
+	JPanel panelBTN = new JPanel();
+	
+	
 }
