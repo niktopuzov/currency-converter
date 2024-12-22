9d570ba5874459f0261873ee2b538afb3f6d76da
Nikola Topuzov <nik.topuzov@gmail.com>
2024-12-22 10:34:23 +0200

    add currency dropdowns and change text fields and labels

--- src/CurrencyConverterGUI.java
+++ src/CurrencyConverterGUI.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,13 +15,14 @@ public class CurrencyConverterGUI extends JFrame {
 	JButton btnCalculate = new JButton("Calculate");
 	JButton btnDelete = new JButton("Delete");
 
-	JTextField textFieldEURO = new JTextField();
-	JTextField textFieldUSD = new JTextField();
-	JTextField textFieldExchangeRate = new JTextField("0.95");
-
-	JLabel euroL = new JLabel("Euro");
-	JLabel usdL = new JLabel("USD");
-	JLabel exchangeRateL = new JLabel("Exchange Rate");
+	JTextField textFieldAmount = new JTextField();
+	JTextField textFieldConvertAmount = new JTextField();
+	
+	JLabel amountLabel = new JLabel("Amount");
+	JLabel convertedAmountLabel = new JLabel("Converted Amount");
+	
+	JComboBox<String> comboBoxFromCurrency = new JComboBox<String>();
+	JComboBox<String> comboBoxToCurrency = new JComboBox<String>();
 
 	JPanel panel = new JPanel();
 	JPanel panelBTN = new JPanel();
@@ -31,6 +33,7 @@ public class CurrencyConverterGUI extends JFrame {
 		this.setSize(400, 250);
 		this.setLayout(new BorderLayout());
 
+		/*
 		panel.setLayout(new GridLayout(3, 2));
 		panel.add(euroL);
 		panel.add(textFieldEURO);
@@ -38,6 +41,7 @@ public class CurrencyConverterGUI extends JFrame {
 		panel.add(textFieldUSD);
 		panel.add(exchangeRateL);
 		panel.add(textFieldExchangeRate);
+		*/
 
 		panelBTN.add(btnCalculate);
 		panelBTN.add(btnDelete);
